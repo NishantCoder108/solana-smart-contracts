93dd3cad6d86d40c8e423efc0785f74fb3d20cb1
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-12-15 15:58:30 +0530

    func: add other 2 test cases for vault anchor program
    - Updated `litesvm.test.ts` to increase deposit amounts and added assertions for new scenarios.
    - Implemented tests to reject zero lamport deposits and withdrawals exceeding vault balance.
    - Introduced a custom error expectation function to validate transaction failures.

--- vault/tests/litesvm.test.ts
+++ vault/tests/litesvm.test.ts
@@ -1,4 +1,4 @@
-import { LiteSVM } from "litesvm";
+import { FailedTransactionMetadata, LiteSVM } from "litesvm";
 import { Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction, TransactionInstruction } from "@solana/web3.js";
 import anchor from "@coral-xyz/anchor";
 import Idl from "../target/idl/vault.json" with {type: "json"};
@@ -11,7 +11,7 @@ describe("LiteSVM: Anchor Vault", () => {
     const coder = new anchor.BorshCoder(Idl as anchor.Idl);
 
     const payer = Keypair.generate();
-    svm.airdrop(payer.publicKey, BigInt(5 * LAMPORTS_PER_SOL));
+    svm.airdrop(payer.publicKey, BigInt(10 * LAMPORTS_PER_SOL));
 
     const programPath = new URL("../target/deploy/vault.so", import.meta.url).pathname;
     svm.addProgramFromFile(programId, programPath);
@@ -22,6 +22,8 @@ describe("LiteSVM: Anchor Vault", () => {
     const [userVaultLamports] = PublicKey.findProgramAddressSync(
         [Buffer.from("user_lamports"), payer.publicKey.toBuffer(), userVault.toBuffer()], programId
     );
+
+
 
     /// Initialize 
     it("Initialize User Vault account", () => {
@@ -63,7 +65,7 @@ describe("LiteSVM: Anchor Vault", () => {
     /// Deposit 
     it("Deposit Lamports to Vault account", () => {
         const ixArgs = {
-            amount: new anchor.BN(LAMPORTS_PER_SOL)
+            amount: new anchor.BN(2 * LAMPORTS_PER_SOL)
         }
         const data = coder.instruction.encode("deposit", ixArgs);
 
@@ -92,11 +94,11 @@ describe("LiteSVM: Anchor Vault", () => {
         // console.log({ userVaultAccount });
 
         const userLampAccount = svm.getBalance(userVaultLamports);
-        // console.log({userLampAccount});
+        // console.log({ userLampAccount });
 
-        assert.equal(userVaultAccount.total_deposit, LAMPORTS_PER_SOL);
+        assert.equal(userVaultAccount.total_deposit, 2 * LAMPORTS_PER_SOL);
         assert.equal(userVaultAccount.user_vault_lamports.toBase58(), userVaultLamports.toBase58());
-        assert.equal(Number(userLampAccount), LAMPORTS_PER_SOL);
+        assert.equal(Number(userLampAccount), 2 * LAMPORTS_PER_SOL);
     })
 
     /// Withdraw
@@ -133,9 +135,54 @@ describe("LiteSVM: Anchor Vault", () => {
         const userLampAccount = svm.getBalance(userVaultLamports);
         // console.log({ userLampAccount });
 
-        assert.equal(userVaultAccount.total_deposit, LAMPORTS_PER_SOL);
+        assert.equal(userVaultAccount.total_deposit, 2 * LAMPORTS_PER_SOL);
         assert.equal(userVaultAccount.user_vault_lamports.toBase58(), userVaultLamports.toBase58());
-        assert.equal(Number(userLampAccount), 0);
+        assert.equal(Number(userLampAccount), LAMPORTS_PER_SOL);
+    })
+
+    /// Reject invalid deposit amounts
+    it("Rejects zero lamport deposit", () => {
+        const data = coder.instruction.encode("deposit", { amount: new anchor.BN(0) });
+        const ix = new TransactionInstruction({
+            keys: [
+                { pubkey: payer.publicKey, isSigner: true, isWritable: true },
+                { pubkey: userVault, isSigner: false, isWritable: true },
+                { pubkey: userVaultLamports, isSigner: false, isWritable: true },
+                { pubkey: SystemProgram.programId, isSigner: false, isWritable: false }
+            ],
+            programId,
+            data
+        });
+        const tx = new Transaction().add(ix);
+        tx.feePayer = payer.publicKey;
+        tx.recentBlockhash = svm.latestBlockhash();
+        tx.sign(payer);
+
+        const res = svm.sendTransaction(tx);
+        // console.log(res.toString())
+        expectCustomError(res, 6004);
+    })
+
+    /// Reject withdrawals that exceed vault balance
+    it("Rejects withdrawing more than available lamports", () => {
+        const data = coder.instruction.encode("withdraw", { amount: new anchor.BN(2 * LAMPORTS_PER_SOL) });
+        const ix = new TransactionInstruction({
+            keys: [
+                { pubkey: payer.publicKey, isSigner: true, isWritable: true },
+                { pubkey: userVault, isSigner: false, isWritable: true },
+                { pubkey: userVaultLamports, isSigner: false, isWritable: true },
+                { pubkey: SystemProgram.programId, isSigner: false, isWritable: false }
+            ],
+            programId,
+            data
+        });
+        const tx = new Transaction().add(ix);
+        tx.feePayer = payer.publicKey;
+        tx.recentBlockhash = svm.latestBlockhash();
+        tx.sign(payer);
+
+        const res = svm.sendTransaction(tx);
+        expectCustomError(res, 6005);
     })
 
     /// Close
@@ -169,4 +216,11 @@ describe("LiteSVM: Anchor Vault", () => {
         assert.equal(Number(userLampAccount), 0);
         assert.equal(userVaultInfo, null);
     })
+
+    const expectCustomError = (res: unknown, code: number) => {
+        assert.instanceOf(res, FailedTransactionMetadata);
+        const errString = (res as FailedTransactionMetadata).err().toString();
+
+        assert.include(errString, `${code}`);
+    };
 })
