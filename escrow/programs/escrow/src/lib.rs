b4caa5b4d41459e11d862565f54d141b7e6fb4f0
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-12-21 01:11:10 +0530

    refactor: streamline escrow function definitions and update context handling
    - Reorganized function definitions in `lib.rs` to improve clarity and maintainability by removing the `pub` visibility modifier from module imports.
    - Updated the `initialize_escrow` function to `maker` and introduced a new `taker` function, enhancing the structure of the escrow program.
    - Adjusted function parameters and added instruction discriminators for better context management in escrow operations.

--- escrow/programs/escrow/src/lib.rs
+++ escrow/programs/escrow/src/lib.rs
@@ -1,8 +1,7 @@
 use anchor_lang::prelude::*;
-pub mod errors;
-pub mod instructions;
-pub mod state;
-
+mod errors;
+mod instructions;
+mod state;
 use instructions::*;
 declare_id!("7ttjDw7MEBJqCaxNc33GozzyH4EHmisaenM8QoyyU4c9");
 
@@ -10,30 +9,29 @@ declare_id!("7ttjDw7MEBJqCaxNc33GozzyH4EHmisaenM8QoyyU4c9");
 pub mod escrow {
     use super::*;
 
-    pub fn initialize_escrow(
-        ctx: Context<InitializeEscrow>,
-        give_mint: Pubkey,
-        give_amount: u64,
-        want_mint: Pubkey,
-        want_amount: u64,
+    #[instruction(discriminator = 0)]
+    pub fn maker(
+        ctx: Context<Maker>,
+        seed: u64,
+        token_mint_n_expected: u64,
+        amount: u64,
     ) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-
-        instructions::initialize::handler(ctx, give_mint, give_amount, want_mint, want_amount)
+        maker::handler(ctx, seed, token_mint_n_expected, amount)
     }
-
-    pub fn deposit_give(ctx: Context<DepositGive>) -> Result<()> {
-        instructions::deposit_give::handler(ctx)
+    #[instruction(discriminator = 1)]
+    pub fn taker(ctx: Context<Taker>) -> Result<()> {
+        taker::handler(ctx)
     }
-
-    pub fn deposit_want<'a>(ctx: Context<'a, 'a, 'a, 'a, DepositWant<'a>>) -> Result<()> {
-        instructions::deposit_want::handler(ctx)
-    }
-
-    // pub fn settle(ctx: Context<instructions::settle::Settle>) -> Result<()> {
-    //     instructions::settle::handler(ctx)
+    // #[instruction(discriminator = 2)]
+    // pub fn refund(ctx: Context<Refund>) -> Result<()> {
+    //     Ok(())
+    //     //...
     // }
-    pub fn cancel(ctx: Context<Cancel>) -> Result<()> {
-        instructions::cancel::handler(ctx)
-    }
+
+    // pub fn take(ctx: Context<Take>) -> Result<()> {
+    //     instructions::take::handler(ctx)
+    // }
+    // pub fn refund(ctx: Context<Refund>) -> Result<()> {
+    //     instructions::refund::handler(ctx)
+    // }
 }
