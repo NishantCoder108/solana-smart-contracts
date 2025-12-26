70f957ba66c5312819a37c4133ccc7f1d024aadb
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-12-27 00:49:40 +0530

    feat: update greeting message in AMM initialization
    - Modified the greeting message in the initialize function of the AMM program to specify "amm" for better clarity during program execution.

--- amm/programs/amm/src/lib.rs
+++ amm/programs/amm/src/lib.rs
@@ -7,7 +7,7 @@ pub mod amm {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        msg!("Greetings from amm: {:?}", ctx.program_id);
         Ok(())
     }
 }
